676e4acb71e9fe398458394dbd151f3ee5df33d2
roshankrsu <roshankrsu@gmail.com>
2025-09-06 08:14:55 +0000

    fixed bmi calculator

--- 5-BMI-Calculator/BMI-Calculator.js
+++ 5-BMI-Calculator/BMI-Calculator.js
@@ -6,15 +6,25 @@ form.addEventListener('submit', function (e) {
   const height = parseInt(document.querySelector('#height').value);
   const weight = parseInt(document.querySelector('#weight').value);
   const results = document.querySelector('#results');
+  const guide = document.querySelector('#bmi-guide'); // Add this element in HTML
 
   if (height === '' || height < 0 || isNaN(height)) {
     results.innerHTML = `Please give a valid height ${height}`;
+    guide.innerHTML = '';
   } else if (weight === '' || weight < 0 || isNaN(weight)) {
     results.innerHTML = `Please give a valid weight ${weight}`;
+    guide.innerHTML = '';
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    
+
     results.innerHTML = `<span>${bmi}</span>`;
+    
+    if (bmi < 18.6) {
+      guide.innerHTML = `You are Underweight `;
+    } else if (bmi >= 18.6 && bmi <= 24.9) {
+      guide.innerHTML = `You are Normal`;
+    } else {
+      guide.innerHTML = `You are Overweight`;
+    }
   }
 });
-
