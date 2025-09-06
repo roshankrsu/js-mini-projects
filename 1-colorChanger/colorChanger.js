587c3ac33fbad9807c96a579a1c2cd0d7c4bf433
roshankrsu <roshankrsu@gmail.com>
2025-09-06 07:01:45 +0000

    color changing project done

--- 1-colorChanger/colorChanger.js
+++ 1-colorChanger/colorChanger.js
@@ -18,6 +18,12 @@ buttons.forEach(function (button) {
     if (e.target.id === 'yellow') {
       body.style.backgroundColor = e.target.id;
     }
+    if(e.target.id ==='purple'){
+      body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id ==='green'){
+      body.style.backgroundColor = e.target.id;
+    }
     
   });
 });
